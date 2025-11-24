940ddbf8c0a64a89366d824cb254d6daf71cff3e
artem korotkov <avkor66@gmail.com>
2025-11-25 01:19:54 +0500

    init: first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {FilmCard} from "./components/film-card/film-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FilmCard
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
